f47682b219afd38e05655ac6920743329ec16a10
SalmanLS <salmanluthfi456@gmail.com>
2023-04-05 16:02:22 +0700

    Tampilan login ke database

--- DL_SS2_C/Program.cs
+++ DL_SS2_C/Program.cs
@@ -10,6 +10,23 @@ namespace DL_SS2_C
     {
         static void Main(string[] args)
         {
+            Program pr = new Program(); 
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database \n");
+                    Console.WriteLine("Masukkan User ID: ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password: ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database Tujuan: ");
+                    string db = Console.ReadLine();
+                    Console.Write("Ketik Y untuk koneksi ke Database!");
+                    char ch = Convert.ToChar(Console.ReadLine());
+
+                }
+            }
         }
     }
 }
