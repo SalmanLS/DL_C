36e465e9b59b412cef096545331af8b8ea72163c
SalmanLS <salmanluthfi456@gmail.com>
2023-04-05 16:06:39 +0700

    koneksi ke database

--- DL_SS2_C/Program.cs
+++ DL_SS2_C/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,20 @@ namespace DL_SS2_C
                     string db = Console.ReadLine();
                     Console.Write("Ketik Y untuk koneksi ke Database!");
                     char ch = Convert.ToChar(Console.ReadLine());
-
+                    switch (ch)
+                    {
+                        case 'Y':
+                            {
+                                SqlConnection konek = null;
+                                string strkonek = "Data source = LUTHFI\\MCH35; " +
+                                    "initial catalog = {0};"+
+                                    "User ID = {1}; Password = {2}";
+                                konek = new SqlConnection(string.Format(strkonek, db, user, pass));
+                                konek.Open();
+                                Console.Clear();
+                            }
+                            break;
+                    }
                 }
             }
         }
